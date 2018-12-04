b826c25ee99a6d3e894c6fb5d4f62b4cdb2ede4e
Mizumaki <31976113+Mizumaki@users.noreply.github.com>
2018-12-04 09:46:25 +0900

    add commentFetchEpic export

--- src/states/ducks/comments/index.ts
+++ src/states/ducks/comments/index.ts
@@ -1,4 +1,4 @@
 export { default as commentsSelectors } from './selectors';
 export { default as commentsOperations } from './operations';
-
+export { default as commentsFetchEpic } from './epics';
 export { default as default, ICommentsState } from './reducers';
